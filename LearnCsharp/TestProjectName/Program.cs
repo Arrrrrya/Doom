d3ed4bfd095e52b9c729fae866cf4fd2ccba5dd6
sun <269404001@qq.com>
2019-02-15 20:09:22 +0800

    新增Console练习

---
+++ LearnCsharp/TestProjectName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectName {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("hello jessica");
+            Console.ReadKey(true);
+        }
+    }
+}
