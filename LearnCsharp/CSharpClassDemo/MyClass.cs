a44662735c74532924566eeed671b348b994c86a
sun <269404001@qq.com>
2019-08-08 12:52:28 +0800

    add classDemo

---
+++ LearnCsharp/CSharpClassDemo/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClassDemo {
+    class MyClass {
+        public string name = "name0";
+        public int age = 0;
+
+        public string Name {
+            get {
+                return name;
+            }
+        }
+
+        public int Age {
+            get {
+                return age;
+            }
+            set {
+                if(value < 0) {
+                    age = 0;
+                } else {
+                    age = value;
+                }
+            }
+
+        }
+    }
+}
