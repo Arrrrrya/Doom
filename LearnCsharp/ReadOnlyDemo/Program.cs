c867be94f52881e180aefc6604d2ecc0b387fb4f
sun <269404001@qq.com>
2019-08-12 22:16:35 +0800

    add ReadOnly and StaticDemo

---
+++ LearnCsharp/ReadOnlyDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyDemo {
+    class Program {
+        static void Main(string[] args) {
+            DateTime date = new DateTime(2019, 08, 12);
+            date.AddDays(1);
+            Console.WriteLine(date);
+            Console.WriteLine(date.AddDays(1));
+
+            string str = "hello";
+            str.ToUpper();
+            Console.WriteLine(str);
+            Console.WriteLine(str.ToUpper());
+
+            StaticDemo s = null;
+            for(int i = 0; i < 100; i++) {
+                s = new StaticDemo();
+                s.increase();
+            }
+            Console.WriteLine("dynamicValue = " + s.dynamicValue);
+            Console.WriteLine("staticValue = " + StaticDemo.staticValue);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    // 类的实例方法，可以访问类的实例字段和静态成员
+    // 类的静态方法，只能访问类的静态字段
+
+    class StaticDemo {
+        public int dynamicValue = 0;
+        public static int staticValue = 0;
+
+        public void increase() {
+            dynamicValue++;
+            staticValue++;
+        }
+
+        public static void staticIncrease() {
+            StaticDemo staticDemo = new StaticDemo();
+            staticDemo.increase();
+            staticDemo.dynamicValue++;
+        }
+    }
+}
