f2442bf73ddf74a1c0aaa7d712342d1d9aa1e84c
sun <269404001@qq.com>
2019-08-26 22:17:55 +0800

    add abstractDemo and interfaceDemo

---
+++ LearnCsharp/InterfaceDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo {
+    class Program {
+        static void Main(string[] args) {
+            Duck d = new Duck();
+            d.fly();
+            d.cook();
+            d.swim();
+
+            Console.ReadKey();
+        }
+    }
+    public abstract class Bird {
+        public abstract void fly();
+    }
+    public interface IFood {
+        void cook();
+    }
+    public interface ISwim {
+        void swim();
+    }
+    public class Duck : Bird, IFood, ISwim {
+        public override void fly() {
+            Console.WriteLine("i'm a duck and i can fly");
+        }
+        public void cook() {
+            Console.WriteLine("i'm a duck and i can be cooked");
+        }
+        public void swim() {
+            Console.WriteLine("i'm a duck and i can swim");
+        }
+    }
+}
---
+++ LearnCsharp/abstractDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractDemo {
+    class Program {
+        static void Main(string[] args) {
+            Fruit f;
+            f = new Apple();
+            f.printLivingArea();
+            f.eatFruit();
+
+            f = new Pineapple();
+            f.printLivingArea();
+            f.eatFruit();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Fruit {
+        public abstract void printLivingArea();
+        public void eatFruit() {
+            Console.WriteLine("i'm not guuuud to eat.");
+        }
+    }
+
+    class Apple : Fruit {
+        public override void printLivingArea() {
+            Console.WriteLine("apple lives in The US.");
+        }
+    }
+
+    class Pineapple : Fruit {
+        public override void printLivingArea() {
+            Console.WriteLine("pineapple lives in Singapore");
+        }
+    }
+}
