072d145440df3b7225082bcc54fb2a2fa695d1ff
sun <269404001@qq.com>
2019-11-26 15:55:05 +0800

    添加sqlite及json使用demo

---
+++ JsonDemo/JsonDemo/DBTools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace JsonDemo {
+    class DBTools
+    {
+        private static SQLiteConnection connection = CreateDatabaseConnection();
+        
+        //与指定的数据库(实际上就是一个文件)建立连接
+        private static SQLiteConnection CreateDatabaseConnection()
+        {
+            string ConnecitonString = System.Configuration.ConfigurationManager.ConnectionStrings["ClientDBConn"].ConnectionString;
+            return new SQLiteConnection(ConnecitonString);
+        }
+
+        // 打开数据库
+        private static void Open(SQLiteConnection connection)
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        // 获得SqlCommand对象
+        public static SQLiteCommand getSQLiteCommand(string sql)
+        {
+            Open(connection);
+            return new SQLiteCommand(sql, connection);
+        }
+
+        // 获得SQLiteDataReader对象
+        public static SQLiteDataReader getSQLiteDataReader(SQLiteCommand command)
+        {
+            SQLiteDataReader reader = null;
+            try
+            {
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return reader;
+        }
+            
+    }
+}
---
+++ JsonDemo/JsonDemo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace JsonDemo {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string sql = System.Configuration.ConfigurationManager.AppSettings.Get("BehaviorSelect");
+        private string[] fields = { "ID", "IDUser", "IDResource", "DateUpload", "BlnBackup", "Remark" };
+        private DataTable dt = new DataTable();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (string fieldName in fields)
+            {
+                dt.Columns.Add(fieldName, Type.GetType("System.String"));
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteCommand command = DBTools.getSQLiteCommand(sql);
+            SQLiteDataReader reader = DBTools.getSQLiteDataReader(command);
+            
+            if (reader != null)
+            {
+                DataRow dr;
+                while (reader.Read())
+                {
+                    dr = dt.NewRow();
+                    foreach (string fieldName in fields)
+                    {
+                        dr[fieldName] = reader[fieldName].ToString();
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            string jsonString = dt.Rows.Count > 0 ? JsonConvert.SerializeObject(dt) : "";
+            Console.WriteLine(jsonString);
+            Console.WriteLine(dt.Rows.Count);
+        }
+    }
+}
