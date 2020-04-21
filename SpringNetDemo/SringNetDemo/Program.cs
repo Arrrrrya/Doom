e6ef19cf59797ca1b66370cd470f5117a4e7a1ea
arya <269404001@qq.com>
2020-04-21 17:30:13 +0800

    添加 spring.net 的Demo

---
+++ SpringNetDemo/SringNetDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+
+namespace SringNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //spring.net读取配置文件，获取上下文
+            IApplicationContext context = ContextRegistry.GetContext();
+            //从配置文件中读取name属性为user的对象实例，并向上转型为User实例
+            User user = context.GetObject("user") as User;
+            Console.WriteLine("userName:\t" + user.userName + "\r\nuserPassword:\t" + user.userPassword);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SpringNetDemo/SringNetDemo/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SringNetDemo
+{
+    public class User
+    {
+        public string userName { set; get; }
+        public string userPassword { set; get; }
+    }
+}
