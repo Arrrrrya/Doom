514a4b9c962a1a6f0c3fe9d3c9d98bf9681d5104
arya <269404001@qq.com>
2020-04-21 11:08:07 +0800

    新增WinApp自动化测试Demo

---
+++ BoringNews/BoringNewsTests/MainWindowTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoringNewsTests;
+using System.Threading;
+
+namespace WpfApplication1.Tests
+{
+    [TestClass()]
+    public class MainWindowTests:TestSessions
+    {
+        [TestMethod]
+        public void Test()
+        {
+            // Find the buttons by their names and click them in sequence to perform 1 + 7 = 8
+            session.FindElementByAccessibilityId("button_Create").Click();
+            Thread.Sleep(2000);
+            session.FindElementByAccessibilityId("button_Copy").Click();
+            Assert.AreEqual("0", "0");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
