465ec31081a0366638d78efd22472057d6c5d49b
arya <269404001@qq.com>
2020-04-22 10:37:03 +0800

    新增一个类，以及试错测试

--- SpringNetDemo/SringNetDemo/Program.cs
+++ SpringNetDemo/SringNetDemo/Program.cs
@@ -4,15 +4,30 @@ using System;
 
 namespace SringNetDemo
 {
+    /// <summary>
+    /// 通过注入的方法为对象和属性赋值
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
-        { //spring.net读取配置文件，获取上下文
-            IApplicationContext context = ContextRegistry.GetContext();
-            //从配置文件中读取name属性为user的对象实例，并向上转型为User实例
-            User user = context.GetObject("user") as User;
+        {
+            IApplicationContext context = ContextRegistry.GetContext();// spring.net读取配置文件，获取上下文
+            User user = context.GetObject("user") as User;// 从配置文件中读取name属性为user的对象实例，并向上转型为User实例
             Console.WriteLine("userName:\t" + user.userName + "\r\nuserPassword:\t" + user.userPassword);
+
+            Skills skill = context.GetObject("skills") as Skills;
+            Console.WriteLine("\r\nskillName:\t" + skill.skillName + "\r\nskillDescribe:\t" + skill.skillDescribe);
+
+            Skills skill_2 = context.GetObject("skills_2") as Skills;
+            Console.WriteLine("\r\nskillName:\t" + skill_2.skillName + "\r\nskillDescribe:\t" + skill_2.skillDescribe);
+
             Console.ReadKey();
         }
+    }
+
+    class Skills
+    {
+        public string skillName { get; set; }
+        public string skillDescribe { get; set; }
     }
 }
