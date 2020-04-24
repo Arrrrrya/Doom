0e7dbe857213a45461f9cf74daaae71ab5b3a28c
arya <269404001@qq.com>
2020-04-24 12:06:37 +0800

    新增for循环测试

---
+++ ForDemo/ForDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1001; i++)
+            {
+                Thread.Sleep(1);
+            }
+            sw.Stop();
+            Console.WriteLine("for i< 用时：\t" + (sw.Elapsed.Milliseconds));
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i <= 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+            sw.Stop();
+            Console.WriteLine("for i<= 用时：\t" + (sw.Elapsed.Milliseconds));
+            int[] array = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                array[i] = i;
+            }
+            sw.Reset();
+            sw.Start();
+            foreach (int i in array)
+            {
+                Thread.Sleep(1);
+            }
+            sw.Stop();
+            Console.WriteLine("foreach 用时：\t" + (sw.Elapsed.Milliseconds));
+            sw.Reset();
+            sw.Start();
+            Parallel.For(0, 1000, i =>
+            {
+                Thread.Sleep(1);
+            });
+            Console.WriteLine("Parallel.For 用时：\t" + (sw.Elapsed.Milliseconds));
+
+            sw.Start();
+            Parallel.ForEach(array, item =>
+            {
+                Thread.Sleep(1);
+            });
+            Console.WriteLine("Parallel.ForEach 用时：\t" + (sw.Elapsed.Milliseconds));
+
+            Console.ReadKey();
+        }
+    }
+}
