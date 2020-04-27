303eb31c1ac8eab737c988823958caf886fbf051
arya <269404001@qq.com>
2020-04-27 17:08:36 +0800

    新增自定义弹窗demo

---
+++ MessageBoxDemo/MessageBoxDemo/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace MessageBoxDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            startListening();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("这是系统弹窗", "提示：", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //MyMessageBox.getMMB().show("hehe");
+
+            MyMessageBox mmb = new MyMessageBox();
+            //mmb.show("hehe");
+            //mmb.show("hehe","只是个提示");
+            //new MyMessageBox().show("hehe");
+            //new MyMessageBox().show("hehe","只是个提示");
+            new MyMessageBox().show("hehe", "按钮1","按钮2","只是个提示");
+            endFlag = true;
+        }
+        
+        bool endFlag = false;
+        void startListening()
+        {
+            DispatcherTimer timer = new DispatcherTimer()
+            {
+                Interval = new System.TimeSpan(0, 0, 0, 1)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            if (endFlag)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ MessageBoxDemo/MessageBoxDemo/MyMessageBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace MessageBoxDemo
+{
+    /// <summary>
+    /// MyMessageBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyMessageBox : Window
+    {
+        private static MyMessageBox mmb = new MyMessageBox();
+
+        public MyMessageBox()
+        {
+            InitializeComponent();
+            Title = "";
+            tip.Content = "";
+            btns.Visibility = Visibility.Collapsed;
+        }
+
+        public static MyMessageBox getMMB()
+        {
+            return mmb;
+        }
+
+        public void show(string msg,string title = "")
+        {
+            Title = title;
+            tip.Content = msg;
+            ShowDialog();
+        }
+
+        public void show(string msg, string btn1, string btn2, string title = "")
+        {
+            Title = title;
+            tip.Content = msg;
+            btns.Visibility = Visibility.Visible;
+            button.Content = btn1;
+            button2.Content = btn2;
+            ShowDialog();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
