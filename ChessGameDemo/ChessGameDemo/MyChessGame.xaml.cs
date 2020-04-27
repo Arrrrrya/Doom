25f47d1ad18765f85578fd92cc70ad48a0aedd6f
arya <269404001@qq.com>
2020-04-27 14:09:00 +0800

    优化获胜判断

--- ChessGameDemo/ChessGameDemo/MyChessGame.xaml.cs
+++ ChessGameDemo/ChessGameDemo/MyChessGame.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ChessGameDemo
 {
@@ -25,11 +27,13 @@ namespace ChessGameDemo
         #region 初始化游戏点击事件
         void initializeGameControl()
         {
-            foreach(Canvas canvas in chess_border.Children)
+            foreach (Canvas canvas in chess_border.Children)
             {
                 canvas.MouseDown += WrapPanel_MouseDown;
             }
+            end_pic.Background = Brushes.Transparent;
             last_winner.Text = "";
+            last_winner_step.Text = "";
         }
         #endregion
 
@@ -65,7 +69,6 @@ namespace ChessGameDemo
         private void WrapPanel_MouseDown(object sender, MouseButtonEventArgs e)
         {
             currentCanvas = sender as Canvas;
-            //MessageBox.Show(currentCanvas.Name);
             if (currentCanvas.Children.Count == 0)
             {
                 if (current_player.Text == player1)
@@ -90,11 +93,13 @@ namespace ChessGameDemo
                 currentCanvas.Children.Add(currentTextBlock);
                 current_player.Text = current_player.Text == player1 ? player2 : player1;
 
-                if (checkCurrent(currentCanvas.Name) != "")
+                if (checkWin())
                 {
                     current_winner.Text = current_player.Text == player1 ? player2 : player1;
                     last_winner.Text = current_winner.Text + " 获胜";
-                    MessageBox.Show("游戏结束！");
+                    last_winner_step.Text = current_winner.Text == player1 ? "所用步数：" + total_step_O.Text + "" : "所用步数：" + total_step_X.Text + "";
+                    MessageBox.Show("游戏结束，再来一局吧!");
+                    showLastGamePic();
                     initializeGamePanel();
                 }
             }
@@ -102,79 +107,135 @@ namespace ChessGameDemo
         #endregion
 
         #region 检查是否有玩家获胜
-        Canvas[] heng = new Canvas[5];
-        string[] shu = new string[5];
-        string[] xie1 = new string[5];
-        string[] xie2 = new string[5];
-        int x;
-        int y;
-        List<string> result = new List<string>();
-        string checkCurrent(string currentCanvasName)
+        Canvas[] resultCanvas = new Canvas[5];
+        List<string> resultList = new List<string>();
+        bool checkWin()
         {
-            resetCheckArray();
-            result.Clear();
-
-            x = Convert.ToInt32(currentCanvasName.Split('_')[1]);
-            y = Convert.ToInt32(currentCanvasName.Split('_')[2]);
-
-            if (x - 2 > 0 && x + 2 < 20 && y - 2 > 0 && y + 2 < 20)
+            if (checkHorizentol() || checkVertical() || checkOblique_1() || checkOblique_2())
             {
-                heng[0] = findByName("canvas_" + (x - 2) + "_" + (y));
-                heng[1] = findByName("canvas_" + (x - 1) + "_" + (y));
-                heng[2] = findByName(currentCanvasName);
-                heng[3] = findByName("canvas_" + (x + 1) + "_" + (y));
-                heng[4] = findByName("canvas_" + (x + 2) + "_" + (y));
-
-                for (int i = 0; i < heng.Length; i++)
+                return true;
+            }
+            return false;
+        }
+        bool checkHorizentol()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 16; j++)
                 {
-                    if (heng[i].Children.Count > 0)
+                    resultList.Clear();
+                    resultCanvas[0] = chess_border.Children[(i) * 20 + j] as Canvas;
+                    resultCanvas[1] = chess_border.Children[(i) * 20 + j + 1] as Canvas;
+                    resultCanvas[2] = chess_border.Children[(i) * 20 + j + 2] as Canvas;
+                    resultCanvas[3] = chess_border.Children[(i) * 20 + j + 3] as Canvas;
+                    resultCanvas[4] = chess_border.Children[(i) * 20 + j + 4] as Canvas;
+                    if (checkResultList(resultCanvas))
                     {
-                        Console.WriteLine(((TextBlock)heng[i].Children[0]).Text);
-                        result.Add(((TextBlock)heng[2].Children[0]).Text);
-                    }
-                    else
-                    {
-                        Console.WriteLine("?");
-                        result.Add("?");
+                        return true;
                     }
                 }
             }
-            else
-            {
-
-            }
-
-            if (result.Count == 5 && !result.Contains("?"))
-            {
-                return currentCanvasName;
-            }
-
-            return "";
+            return false;
         }
-        void resetCheckArray()
-        {
-            for (int i = 0; i < 5; i++)
+        bool checkVertical()
+        {;
+            for (int i = 0; i < 16; i++)
             {
-                heng[i] = null;
-                shu[i] = "";
-                xie1[i] = "";
-                xie2[i] = "";
-            }
-        }
-        Canvas findByName(string name)
-        {
-            foreach (Canvas canvas in chess_border.Children)
-            {
-                if (canvas.Name == name)
+                for (int j = 0; j < 20; j++)
                 {
-                    return canvas;
+                    resultList.Clear();
+                    resultCanvas[0] = chess_border.Children[(i) * 20 + j] as Canvas;
+                    resultCanvas[1] = chess_border.Children[(i + 1) * 20 + j] as Canvas;
+                    resultCanvas[2] = chess_border.Children[(i + 2) * 20 + j] as Canvas;
+                    resultCanvas[3] = chess_border.Children[(i + 3) * 20 + j] as Canvas;
+                    resultCanvas[4] = chess_border.Children[(i + 4) * 20 + j] as Canvas;
+                    if (checkResultList(resultCanvas))
+                    {
+                        return true;
+                    }
                 }
             }
-            return null;
+            return false;
         }
-
+        bool checkOblique_1()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    resultList.Clear();
+                    resultCanvas[0] = chess_border.Children[(i) * 20 + j] as Canvas;
+                    resultCanvas[1] = chess_border.Children[(i + 1) * 20 + j + 1] as Canvas;
+                    resultCanvas[2] = chess_border.Children[(i + 2) * 20 + j + 2] as Canvas;
+                    resultCanvas[3] = chess_border.Children[(i + 3) * 20 + j + 3] as Canvas;
+                    resultCanvas[4] = chess_border.Children[(i + 4) * 20 + j + 4] as Canvas;
+                    if (checkResultList(resultCanvas))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        bool checkOblique_2()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 4; j < 20; j++)
+                {
+                    resultList.Clear();
+                    resultCanvas[0] = chess_border.Children[(i) * 20 + j] as Canvas;
+                    resultCanvas[1] = chess_border.Children[(i + 1) * 20 + j - 1] as Canvas;
+                    resultCanvas[2] = chess_border.Children[(i + 2) * 20 + j - 2] as Canvas;
+                    resultCanvas[3] = chess_border.Children[(i + 3) * 20 + j - 3] as Canvas;
+                    resultCanvas[4] = chess_border.Children[(i + 4) * 20 + j - 4] as Canvas;
+                    if (checkResultList(resultCanvas))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 检查五星连珠
+        /// </summary>
+        /// <param name="resultCanvas"></param>
+        /// <param name="currentCanvasName"></param>
+        /// <returns></returns>
+        bool checkResultList(Canvas[] resultCanvas)
+        {
+            for (int i = 0; i < resultCanvas.Length; i++)
+            {
+                if (resultCanvas[i].Children.Count > 0)
+                {
+                    resultList.Add(((TextBlock)resultCanvas[i].Children[0]).Text);
+                }
+                else
+                {
+                    resultList.Add("?");
+                }
+            }
+            if (!resultList.Contains("?") && !(resultList.Contains(player1) && resultList.Contains(player2)))
+            {
+                return true;
+            }
+            return false;
+        }
         #endregion
 
-        
+        #region 保存并显示上局截图
+        void showLastGamePic()
+        {
+            end_pic.Background = new ImageBrush() { ImageSource = screenShotLastGamePic(chess_border, 600, 600) };
+
+        }
+        RenderTargetBitmap screenShotLastGamePic(Visual vsual, int width, int height)
+        {
+            RenderTargetBitmap pic = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
+            pic.Render(vsual);
+            return pic;
+        }
+        #endregion
     }
 }
