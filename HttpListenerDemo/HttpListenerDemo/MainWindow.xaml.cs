abbbae5c42ff5ab1555ce04c20bf357900322b89
arya <269404001@qq.com>
2020-04-28 19:28:57 +0800

    新增http通信demo，实现多用户投票功能

---
+++ HttpListenerDemo/HttpListenerDemo/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace HttpListenerDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string test { get; set; } = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            refreshTextBlock();
+        }
+
+        string url = "http://127.0.0.1:8888/";
+        //string url = "http://192.168.1.3/";
+
+        private void checkListen_Checked(object sender, RoutedEventArgs e)
+        {
+            test = exercise.Text;
+            Task.Factory.StartNew(() =>
+            {
+                HttpListener listener = new HttpListener();
+                while (true)
+                {
+                    try
+                    {
+                        listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;//指定身份验证 Anonymous匿名访问
+                        listener.Prefixes.Add(url);
+                        listener.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("服务启动失败..." + ex.Message);
+                        break;
+                    }
+                    Console.WriteLine("服务器启动成功.......");
+
+                    //线程池
+                    int minThreadNum;
+                    int portThreadNum;
+                    int maxThreadNum;
+                    ThreadPool.GetMaxThreads(out maxThreadNum, out portThreadNum);
+                    ThreadPool.GetMinThreads(out minThreadNum, out portThreadNum);
+                    Console.WriteLine("最大线程数：{0}", maxThreadNum);
+                    Console.WriteLine("最小空闲线程数：{0}", minThreadNum);
+                    //ThreadPool.QueueUserWorkItem(new WaitCallback(TaskProc1), x);
+
+                    Console.WriteLine("\n\n等待客户连接中。。。。");
+                    while (true)
+                    {
+                        //等待请求连接
+                        //没有请求则GetContext处于阻塞状态
+                        HttpListenerContext ctx = listener.GetContext();
+                        ThreadPool.QueueUserWorkItem(new WaitCallback(TaskProc), ctx);
+                    }
+                }
+            });
+        }
+
+        void TaskProc(object o)
+        {
+            try
+            {
+                HttpListenerContext ctx = (HttpListenerContext)o;
+
+                ctx.Response.StatusCode = 200;//设置返回给客服端http状态代码
+
+                //接收POST参数
+                Stream stream = ctx.Request.InputStream;
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                string body = reader.ReadToEnd();
+                text = "收到POST数据:" + body;
+                text = body;
+                Console.WriteLine(text);
+                isNeedFresh = true;
+
+                //使用Writer输出http响应代码,UTF8格式
+                using (StreamWriter writer = new StreamWriter(ctx.Response.OutputStream, Encoding.UTF8))
+                {
+                    writer.Write(test);
+                    writer.Close();
+                    ctx.Response.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        bool isNeedFresh = false;
+        string text = "";
+        DispatcherTimer timer = new DispatcherTimer();//计时器，用于刷新显示文字
+        void refreshTextBlock()
+        {
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);//测试每0.1秒执行一次
+            timer.Tick += (object sender, EventArgs e) =>
+            {
+                if (isNeedFresh)
+                {
+                    if (text == "c")
+                    {
+                        bar_c.Value += 10;
+                    }
+                    else if (text == "b")
+                    {
+                        bar_b.Value += 10;
+                    }
+                    else if (text == "a")
+                    {
+                        bar_a.Value += 10;
+                    }
+                    isNeedFresh = false;
+                }
+            };
+            timer.Start();
+        }
+    }
+}
---
+++ HttpListenerDemo/MyHttpClient/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyHttpClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string url = "http://127.0.0.1:8888/";
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox.Text = url;
+        }
+
+        #region 单一用户 瞬间发送多次消息
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (button.Content.ToString() == "连接")
+            {
+                test = HttpPost(url, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss  " + "用户连接成功！"));
+                button.Content = "发送";
+                label.Content = test;
+            }
+            else
+            {
+                HttpPost(url, /*DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss  " + (n == 1 ? "" : i + "") + "  ") + */textBox1.Text);
+
+                //int n = 1;
+                //try { n = Convert.ToInt32(textBox2.Text.Trim()); }
+                //catch (Exception) { n = 1; }
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //    HttpPost(url, /*DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss  " + (n == 1 ? "" : i + "") + "  ") + */textBox1.Text);
+                //}
+                button.IsEnabled = false;
+            }
+
+        }
+        static string test = "";
+        private static string HttpPost(string url, string postDataStr)
+        {
+            //Task.Factory.StartNew(() =>
+            //{
+            try
+            {
+                string strReturn;
+                //在转换字节时指定编码格式
+                byte[] byteData = Encoding.UTF8.GetBytes(postDataStr);
+
+                //配置Http协议头
+                HttpWebRequest resquest = (HttpWebRequest)WebRequest.Create(url);
+                resquest.Method = "POST";
+                resquest.ContentType = "application/x-www-form-urlencoded";
+                resquest.ContentLength = byteData.Length;
+
+                //发送数据
+                using (Stream resquestStream = resquest.GetRequestStream())
+                {
+                    resquestStream.Write(byteData, 0, byteData.Length);
+                }
+
+                //接受并解析信息
+                using (WebResponse response = resquest.GetResponse())
+                {
+                    //解决乱码：utf-8 + streamreader.readToEnd
+                    StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                    strReturn = reader.ReadToEnd();
+                    test = strReturn;
+                    reader.Close();
+                    reader.Dispose();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                MessageBox.Show(e.Message);
+            }
+            //});
+            return test;
+        }
+        #endregion
+
+        #region 多用户同时访问
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            int n = 1;
+            //try { n = Convert.ToInt32(textBox3.Text.Trim()); }
+            //catch (Exception) { n = 1; }
+
+            List<User> userList = new List<User>();
+            for (int i = 0; i < n; i++)
+            {
+                userList.Add(new User(i + 1));
+            }
+
+            // 利用并行循环模拟多用户操作
+            Parallel.ForEach(userList, (user, loopState) =>
+            {
+                user.sendMsg();
+            });
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// 模拟多用户
+    /// </summary>
+    class User
+    {
+        string url = "http://192.168.0.110/";
+        int userId = 0;
+
+        public User(int userId)
+        {
+            this.userId = userId;
+        }
+
+        public void sendMsg()
+        {
+            HttpPost(url, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss  " + "我是用户" + userId));
+        }
+
+        #region 发送消息
+        public static void HttpPost(string url, string postDataStr)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    string strReturn;
+                    //在转换字节时指定编码格式
+                    byte[] byteData = Encoding.UTF8.GetBytes(postDataStr);
+
+                    //配置Http协议头
+                    HttpWebRequest resquest = (HttpWebRequest)WebRequest.Create(url);
+                    resquest.Method = "POST";
+                    resquest.ContentType = "application/x-www-form-urlencoded";
+                    resquest.ContentLength = byteData.Length;
+
+                    //发送数据
+                    using (Stream resquestStream = resquest.GetRequestStream())
+                    {
+                        resquestStream.Write(byteData, 0, byteData.Length);
+                    }
+
+                    //接受并解析信息
+                    using (WebResponse response = resquest.GetResponse())
+                    {
+                        //解决乱码：utf-8 + streamreader.readToEnd
+                        StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                        strReturn = reader.ReadToEnd();
+                        reader.Close();
+                        reader.Dispose();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    MessageBox.Show(e.Message);
+                }
+            });
+        }
+        #endregion
+    }
+}
